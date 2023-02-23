5632c9cdf0b478d6dd79092291afe8ace0c3246c
Kristian <kr.mendoza.sh@gmail.com>
2023-02-22 20:06:02 -0500

    completed all functions in js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let operator;
-let array=[0,0];
-let currentNumber=array[0];
+let array=[0];
+showNumber()
+/*array [0] will be the first number and array[1] will be the second number
+after calculation, array [1] will be copied to array[0], then array [1] will
+be deleted*/
 
 function calculate(){
     numberA=array[0];
     numberB=array[1];
-    if (array.length==1){
-        array[0]=arguments[0];
-    }
-    else if (array.length==2){
+    if (array.length==2){
         if (operator=="add"){
             array[0]=numberA+numberB;
         }
@@ -21,12 +21,62 @@ function calculate(){
         else if (operator=="divide"){
             array[0]=numberA/numberB;
         }
+        array[0]=array[0].toString()
         array.pop();
     }
-    currentNumber=array[0];
-    document.getElementById("screen").innerText=currentNumber;
+    showNumber();
 }
 
+function showNumber(){
+    if(array.length==1){
+        if (document.getElementById("screen").innerText=array[0]==0){
+            document.getElementById("screen").innerText=array[0];
+        }
+        else{
+            document.getElementById("screen").innerText=parseInt(array[0].toString(1));
+        }
+    }
+    else if(array.length==2){
+        if (document.getElementById("screen").innerText=array[1]==0){
+            document.getElementById("screen").innerText=array[1];
+        }
+        else{
+            document.getElementById("screen").innerText=parseInt(array[1].toString(1));
+            }
+    }
+    console.log(array)
+}
+
+
+document.querySelectorAll('button').forEach(item =>{
+    item.addEventListener('click', (e) =>{
+        if (item.id.length>2){ // operator button pressed
+            if (item.id=="clear"){
+                if(array.length==2){
+                    array.pop();
+                }
+                array[0]=0;
+            }
+            if (array.length==2){
+                calculate()
+            }
+            else{
+                array.push(0);
+            }
+            operator=item.id;
+            console.log(operator)
+        }
+        else{ //number button pressed
+            if (array.length==1){
+                array[0]=array[0].toString()+item.id;
+            }
+            else if (array.length==2){
+                array[1]=array[1].toString()+item.id;
+            }
+        }
+        showNumber()
+    })
+})
 
 /*Psuedo code
 listen for buttons,
@@ -37,4 +87,4 @@ make array[0]=array[1] and delete array[1] for future calculatons.
 
 what i need: calculate function
 
-global array
+global array*/
