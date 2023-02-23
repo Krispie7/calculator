a3002a4f655900f6314bf193c664f0f1899b3cf2
Kristian <kr.mendoza.sh@gmail.com>
2023-02-22 19:10:27 -0500

    added psuedocode and calc function

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-function add(a , b){
-    return a+b;
+let operator;
+let array=[0,0];
+let currentNumber=array[0];
+
+function calculate(){
+    numberA=array[0];
+    numberB=array[1];
+    if (array.length==1){
+        array[0]=arguments[0];
+    }
+    else if (array.length==2){
+        if (operator=="add"){
+            array[0]=numberA+numberB;
+        }
+        else if (operator=="subtract"){
+            array[0]=numberA-numberB;
+        }
+        else if (operator=="multiply"){
+            array[0]=numberA*numberB;
+        }
+        else if (operator=="divide"){
+            array[0]=numberA/numberB;
+        }
+        array.pop();
+    }
+    currentNumber=array[0];
+    document.getElementById("screen").innerText=currentNumber;
 }
 
-function subtract(a, b){
-    return a-b;
-}
 
-function multiply(a, b){
-    return a*b;
-}
+/*Psuedo code
+listen for buttons,
+when one button is pressed, save into an array of 2 long, 
+when operator is pressed, store choice in operator variable
+when second number is pressed, save into array[1] and return the answer
+make array[0]=array[1] and delete array[1] for future calculatons.
 
-function divide(a, b){
-    return a/b;
-}
+what i need: calculate function
+
+global array
